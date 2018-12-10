607d3910dc5d007951c0a7c3dc70291c12d246f2
Julian WEBER <julian.weber@hotmail.fr>
2018-12-10 14:46:36 +0100

    Basic socket test

---
+++ visu/index.js
@@ -0,0 +1,20 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/visu.html');
+});
+
+app.get('/event', function(req, res){
+  res.send("Event")
+  io.emit("event")
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+});
+
+http.listen(3000, function(){
+  console.log('listening on *:3000');
+});
