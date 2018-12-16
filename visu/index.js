1da7e659ca974e5c027331c3b46f1ff3b59b8f15
Julian WEBER <julian.weber@hotmail.fr>
2018-12-16 13:29:53 +0100

    adding three.js library

--- visu/index.js
+++ visu/index.js
@@ -1,6 +1,10 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+const fs = require('fs');
+const readLastLines = require('read-last-lines');
+
+file = "./positions.log"
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/visu.html');
@@ -13,6 +17,20 @@ app.get('/event', function(req, res){
 
 io.on('connection', function(socket){
   console.log('a user connected');
+});
+
+fs.watchFile(file, { interval: 100 }, () => {
+  console.log("New pos");
+  readLastLines.read(file, 1)
+    .then((lines) => {
+      lines = lines.split("|")
+      for (var i = 0; i < lines.length; i++){
+        lines[i] = lines[i].split(" ")
+      }
+      io.emit("event", lines)
+    });
+  
+  
 });
 
 http.listen(3000, function(){
