47d958cf27000608a8c3ac5a61bb9665a3963a5f
Julian WEBER <julian.weber@hotmail.fr>
2018-12-31 15:52:48 +0100

    testing writing

--- visu/distAruco.py
+++ visu/distAruco.py
@@ -33,9 +33,9 @@ with picamera.PiCamera() as camera:
                 for id in ids:
                     rvec, tvec = aruco.estimatePoseSingleMarkers(corners[0], MARKER_SIZE, mtx, dist)
                     print("marker " + str(id[0]))
-                    print("x: {0}, y: {1}, z: {2}".format(rvec[0][0][0], rvec[0][0][1], rvec[0][0][2]))
-                    print("rx: {0}, ry: {1}, rz: {2}".format(tvec[0][0][0], tvec[0][0][1], tvec[0][0][2]))
-                    line += "{0} {1} {2} {3}|".format(id[0], rvec[0][0][0], rvec[0][0][1], tvec[0][0][0])
+                    print("x: {0}, y: {1}, z: {2}".format(tvec[0][0][0], tvec[0][0][1], tvec[0][0][2]))
+                    print("rx: {0}, ry: {1}, rz: {2}".format(rvec[0][0][0], rvec[0][0][1], rvec[0][0][2]))
+                    line += "{0} {1} {2} {3}|".format(id[0], tvec[0][0][2], tvec[0][0][0], tvec[0][0][0])
                     print("")
                     i+=1
                 line = line[:-1]
