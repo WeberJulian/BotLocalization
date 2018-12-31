88f573b5c4b5ad7f46f4096c59245de5234ccdd9
Julian WEBER <julian.weber@hotmail.fr>
2018-12-31 15:40:39 +0100

    checking witchparameters to send

--- visu/distAruco.py
+++ visu/distAruco.py
@@ -28,12 +28,17 @@ with picamera.PiCamera() as camera:
             corners, ids, rejectedImgPoints = aruco.detectMarkers(gray, aruco_dict, parameters=parameters)
             if np.all(ids != None):
                 i = 0
+                out = open("positions.log", "a")
+                line = ""
                 for id in ids:
                     rvec, tvec = aruco.estimatePoseSingleMarkers(corners[0], MARKER_SIZE, mtx, dist)
                     print("marker " + str(id[0]))
                     print("x: {0}, y: {1}, z: {2}".format(rvec[0][0][0], rvec[0][0][1], rvec[0][0][2]))
                     print("rx: {0}, ry: {1}, rz: {2}".format(tvec[0][0][0], tvec[0][0][1], tvec[0][0][2]))
+                    line += "{0} {1} {2} {3}|".format(id[0]), rvec[0][0][0], rvec[0][0][1], tvec[0][0][0]))
                     print("")
                     i+=1
+                line = line[:-1]
+                print(line)
             print("\n\n\n")
 
